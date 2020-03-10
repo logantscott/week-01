c369a739ee9b999e5bd8ca2683255d05125394e6
Logan Scott <logantscott@gmail.com>
2020-03-10 14:45:30 -0700

    Validator class WIP

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const { isNumber } = require('./lib/types.js');
+const { castToNumber } = require('./lib/types.js');
+const { Validator } = require('./lib/Validator.js');
 
-console.log(isNumber('3'));
+const nameSchema = {
+  type: String,
+  required: true
+};
+
+const ageSchema = {
+  type: Number,
+  required: true
+};
+
+const weightSchema = {
+  type: String,
+  required: true
+};
+
+const obj = {
+  age: 7,
+  weight: '20 lbs'
+};
+
+const nameValidator = new Validator('name', nameSchema);
+const ageValidator = new Validator('age', ageSchema);
+const weightValidator = new Validator('weight', weightSchema);
+
+// console.log(nameValidator);
+
+console.log(nameValidator.validate(obj), ageValidator.validate(obj), weightValidator.validate(obj));
