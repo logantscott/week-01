2c0e0daa52f722e5dff40ab89bebb6c2f332c410
Logan Scott <logantscott@gmail.com>
2020-03-11 16:11:43 -0700

    copy ryan in class, still broken

--- __tests__/file-system.test.js
+++ __tests__/file-system.test.js
@@ -1,9 +1,11 @@
 const fs = require('fs').promises;
-const { mkdirp } = require('../lib/file-system.js');
+const { mkdirp, writeJSON, readJSON, readDirectoryJSON } = require('../lib/file-system.js');
 
 jest.mock('fs', () => ({
   promises: {
-    mkdir: jest.fn(() => Promise.resolve())
+    mkdir: jest.fn(() => Promise.resolve()),
+    writeFile: jest.fn(() => Promise.resolve()),
+    readFile: jest.fn(() => Promise.resolve('{"name":"spot"}'))
   }
 }));
 
@@ -12,6 +14,30 @@ describe('file system functions', () => {
     return mkdirp('./my/cool/directory/path')
       .then(() => {
         expect(fs.mkdir).toHaveBeenCalledWith('./my/cool/directory/path', { recursive: true });
+      });
+  });
+
+  it('writes an object to a file', () => {
+    const dog = {
+      name: 'spot',
+      age: 5,
+      weight: '20 lbs'
+    };
+
+    // return writeJSON('./test.json', dog)
+    //   .then()
+    expect(fs.writeFile)
+      .toHaveBeenCalledWith('./test.json', JSON.stringify(dog));
+  });
+
+  it('can read an object from a file', () => {
+    return readJSON('./test.json')
+      .then(data => {
+        expect(fs.readFile)
+          .toHaveBeenCalledWith('./test.json', { 'encoding': 'utf8' });
+        expect(data).toEqual({
+          name: 'spot'
+        });
       });
   });
 });
